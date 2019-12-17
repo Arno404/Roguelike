b0dd5e8fc77b9326d12949742ca405a9c0f55cba
Ivan Morozko <vanya.morozko@mail.ru>
2019-12-18 01:43:27 +0300

    Added DumbAI for monsters

--- DumbAI.cs
+++ DumbAI.cs
@@ -10,7 +10,45 @@ namespace Roguelike
     {
         public void FindPath(Monster monster, BaseEntity target)
         {
+            int distanceY = monster.Coords.X - target.Coords.X;
+            int distanceX = monster.Coords.Y - target.Coords.Y;
+            int absX = Math.Abs(distanceX);
+            int absY = Math.Abs(distanceY);
 
+            if (distanceX > 0 && distanceY > 0)
+            {
+                if (absX < absY)
+                    monster.CurrentMoveAction = BaseCharacter.MoveAction.Left;
+                else monster.CurrentMoveAction = BaseCharacter.MoveAction.Up;
+            }
+            if (distanceX < 0 && distanceY > 0)
+            {
+                if (absX < absY)
+                    monster.CurrentMoveAction = BaseCharacter.MoveAction.Left;
+                else monster.CurrentMoveAction = BaseCharacter.MoveAction.Down;
+            }
+            if (distanceX > 0 && distanceY < 0)
+            {
+                if (absX < absY)
+                    monster.CurrentMoveAction = BaseCharacter.MoveAction.Right;
+                else monster.CurrentMoveAction = BaseCharacter.MoveAction.Up;
+            }
+            if (distanceX < 0 && distanceY < 0)
+            {
+                if (absX < absY)
+                    monster.CurrentMoveAction = BaseCharacter.MoveAction.Right;
+                else monster.CurrentMoveAction = BaseCharacter.MoveAction.Down;
+            }
+            if (distanceX == 0)
+            {
+                if(distanceY > 0) monster.CurrentMoveAction = BaseCharacter.MoveAction.Left;
+                else monster.CurrentMoveAction = BaseCharacter.MoveAction.Right;
+            }
+            if (distanceY == 0)
+            {
+                if (distanceX > 0) monster.CurrentMoveAction = BaseCharacter.MoveAction.Up;
+                else monster.CurrentMoveAction = BaseCharacter.MoveAction.Down;
+            }
         }
     }
 }
